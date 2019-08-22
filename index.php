938e0a305bfee8a9d47ec34d338e7b20c590deee
Vladimir Mitrović <v@brutalism.rs>
2019-08-22 14:42:43 +0200

    Врећо

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 <td align="center">
 <?php
 
-$slova = array();
 // jednoslovni clanovi su dvaput uneti, da bi ih bilo "vise" -> verovatnoca da se izabere clan od
 // jednog ili dva slova je priblizno ista
+$slova = array();
 $slova[0] = array("Б", "В", "Г", "Д", "Ђ", "Ж", "З", "Ј", "К", "Л", "Љ", "М", "Н", "Њ", "П", "Р", 
                   "С", "Т", "Ћ", "Ф", "Х", "Ц", "Ч", "Џ", "Ш", "Б", "В", "Г", "Д", "Ђ", "Ж", "З",
                   "Ј", "К", "Л", "Љ", "М", "Н", "Њ", "П", "Р", "С", "Т", "Ћ", "Ф", "Х", "Ц", "Ч",
@@ -25,19 +25,28 @@ $slova[1] = array("а", "е", "и", "о", "у", "р");
 $slova[2] = array("б", "в", "г", "д", "ђ", "ж", "з", "ј", "к", "л", "љ", "м", "н", "њ", "п", "р", 
                   "с", "т", "ћ", "ф", "х", "ц", "ч", "џ", "ш");
 
-srand();
-$t1 = $slova[0][rand(0, count($slova[0])-1)];
-// ako je na kraju prvog clana "r" ili neko ruzno slovo, onda nemoj kao drugi clan da stavljas "r"!
-$a = substr($t1, -2);
-if ($a == "л" || $a == "р" || $a == "ј" || $a == "љ" || $a == "њ" || $a == "Ђ" || 
-    $a == "Ж" || $a == "Ј" || $a == "Л" || $a == "Љ" || $a == "Н" || $a == "Њ" ||
-    $a == "Р" || $a == "Ћ" || $a == "Ч" || $a == "Џ" || $a == "Ш") $dec = 2;
-else $dec = 1;
-$t2 = $slova[1][rand(0, count($slova[1])-$dec)];
-$t3 = $slova[2][rand(0, count($slova[2])-1)];
+function generisi() {
+  global $slova;
+  
+  srand();
+  $t1 = $slova[0][rand(0, count($slova[0])-1)];
+  // ako je na kraju prvog clana "r" ili neko ruzno slovo, onda nemoj kao drugi clan da stavljas "r"!
+  $a = substr($t1, -2);
+  if ($a == "л" || $a == "р" || $a == "ј" || $a == "љ" || $a == "њ" || $a == "Ђ" || 
+      $a == "Ж" || $a == "Ј" || $a == "Л" || $a == "Љ" || $a == "Н" || $a == "Њ" ||
+      $a == "Р" || $a == "Ћ" || $a == "Ч" || $a == "Џ" || $a == "Ш") $dec = 2;
+  else $dec = 1;
+  $t2 = $slova[1][rand(0, count($slova[1])-$dec)];
+  $t3 = $slova[2][rand(0, count($slova[2])-1)];
+
+  return array($t1, $t2, $t3);
+}
+
+list($t1, $t2, $t3) = generisi();
 
 if (isset($_GET["lolo"])) {
 	$t4 = $t3.$t2.$t3."о";
+  echo "<h1>".$t1.$t2.$t3."о$t4 Полумента</h1>";
 }
 else if (isset($_GET["fljizo"])) {
 	$t = "";
@@ -54,17 +63,23 @@ else if (isset($_GET["fljizo"])) {
 	         ($a == "л" || $a == "р" || $a == "ј" || $a == "љ" || $a == "њ" || $a == "ђ" || 
 	          $a == "ж" || $a == "н" || $a == "ћ" || $a == "ч" || $a == "џ" || $a == "ш"));
 	$t4 = $t.$t2.$t3."о";
+  echo "<h1>".$t1.$t2.$t3."о$t4 Полумента</h1>";
 }
-else $t4 = "";
+else if (isset($_GET["vreco"])) {
+  list($t4, $t5, $t6) = generisi();
+  echo "<h1>".$t1.$t2.$t3."o ".$t4.$t5.$t6."o</h1>";
+}
+else {
+  echo "<h1>".$t1.$t2.$t3."о Полумента</h1>";
+}
 
-echo "<h1>".$t1.$t2.$t3."о$t4 Полумента</h1>";
 ?>
 </td>
 </tr>
 <tr><td valign="bottom" align="center"><font size="1">
 	(Кликћи на "Reload" или на неки од линкова испод.)<br>
 	<a href="index.php">Стандард</a> | <a href="index.php?lolo">Лоло</a>
-	 | <a href="index.php?fljizo">Фљизо</a></font></td></tr>
+	 | <a href="index.php?fljizo">Фљизо</a> | <a href="index.php?vreco">Врећо</a></font></td></tr>
 </table>
 </body>
 </html>
